13fedfa9ca298d165ec4d2ba8251f2a12e5737f6
Felipe Ingenleuf Joubeir <felipeingenleuf@gmail.com>
2022-04-26 19:57:22 -0300

    Criação do pagamento e da reserva - comit (terça) felipe

--- src/Pagamento.java
+++ src/Pagamento.java
@@ -1,3 +1,5 @@
 public class Pagamento {
+
+    public double calcularPagamento();
     
 }
--- src/Reserva.java
+++ src/Reserva.java
@@ -1,3 +1,60 @@
-public class Reserva {
+public class Reserva implements Pagamento {
+
+    private Cliente cliente;
+    private boolean PagamentoAVista;
+
+    public Reserva(Cliente cliente, boolean PagamentoAVista){
+        this.cliente = cliente;
+        this.PagamentoAVista = PagamentoAVista;
+    }
     
+    @Override
+    public String toString() {
+        
+        String aux = "";
+        
+        if(cliente instanceof PessoaFisica){
+            aux += " -> Pessoa Física";
+        } else{
+            aux += " -> Pessoa Jurídica"; 
+        }
+
+        aux += "\nCliente: " + cliente.getNome(); 
+
+        if(getpagamentoAVista() == true){
+            aux += "\n-> Pagamento: À vista";
+        } else{
+            aux += "\n-> Pagamento: Parcelado";
+        }
+
+        return aux;
+    }
+
+    @Override
+    public double calcularPagamento(){
+        
+        double valor = 3200.00;
+
+        if(pagamentoAVista == true){
+            return valor * 0.9;
+        } else{
+            return valor;
+        }
+    }
+
+    public boolean getPagamentoAVista() {
+        return this.PagamentoAVista;
+    }
+
+    public void setPagamentoAVista(boolean PagamentoAVista) {
+        this.PagamentoAVista = PagamentoAVista;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
 }
